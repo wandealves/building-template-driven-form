c67b1d1ef15343c8bde97ffb5b4f22a8ab8b04ae
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-11-02 16:12:27 -0300

    form

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeroFormComponent } from "./hero-form/hero-form.component";
+
+@NgModule({
+  declarations: [AppComponent, HeroFormComponent],
+  imports: [BrowserModule, AppRoutingModule, CommonModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
